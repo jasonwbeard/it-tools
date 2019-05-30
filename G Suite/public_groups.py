25512954245f530380a66f1bdac36d0e96640ca5
Jason Beard <jbe@c02y50zwjg5m-c.localdomain>
2019-05-30 15:24:28 -0500

    added tool to check groups for public message posting

--- G Suite/public_groups.py
+++ G Suite/public_groups.py
@@ -3,24 +3,25 @@ from apiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
 
-# Setup the Admin SDK Directory API
-SCOPES = ('https://www.googleapis.com/auth/admin.directory.group.readonly ' +
-          'https://www.googleapis.com/auth/admin.directory.group.member.readonly' +
-          'https://www.googleapis.com/auth/admin.directory.user.readonly')
+SCOPES = ('https://www.googleapis.com/auth/apps.groups.settings ' +
+          'https://www.googleapis.com/auth/admin.directory.group.readonly')
+
+# Build the connections for 1) Admin SDK Directory API and 2) Groups Settings API
 store = file.Storage('credentials.json')
 creds = store.get()
 if not creds or creds.invalid:
     flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
     creds = tools.run_flow(flow, store)
-service = build('admin', 'directory_v1', http=creds.authorize(Http()))
+groups_service = build('admin', 'directory_v1', http=creds.authorize(Http()))
+settings_service = build('groupssettings', 'v1', http=creds.authorize(Http()))
 
 # Initialize the page iterators
 group_nextPageToken = None
 user_nextPageToken = None
 
-# Call the Admin SDK Directory API
+# Iterate over each group
 while True:
-    group_results = service.groups().list(customer='my_customer', maxResults='100', orderBy='email', pageToken=group_nextPageToken).execute()
+    group_results = groups_service.groups().list(customer='my_customer', maxResults='100', orderBy='email', pageToken=group_nextPageToken).execute()
     groups = group_results.get('groups', [])
     group_nextPageToken = group_results.get('nextPageToken', None)
 
@@ -28,20 +29,12 @@ while True:
         print('No groups in the domain.')
     else:
         for group in groups:
-       	# Search the group members for a non-matching email
-            while True:
-                user_results = service.members().list(groupKey=group['email'], maxResults='100', pageToken=user_nextPageToken).execute()
-                users = user_results.get('members', [])
-                user_nextPageToken = user_results.get('nextPageToken', None)
+            # Get the posting setting for a group
+            settings = settings_service.groups()
+            setting = settings.get(groupUniqueId=(group.get('email'))).execute()
 
-                for user in users:
-                    if ("@tradeshift.com" not in user['email'] and 
-                        "@tradeshift.zendesk.com" not in user['email'] and
-                        "@tradeshift.pagerduty.com" not in user['email']):
-                        print('{0}: {1}'.format(group['email'], user['email']))
-           
-                if user_nextPageToken is None:
-                   break
+            if setting.get('whoCanPostMessage') == "ANYONE_CAN_POST":
+                print (group.get('email') + ": " + setting.get('whoCanPostMessage'))
 
     if group_nextPageToken is None:
         break
