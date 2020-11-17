c4290fcc0c4f1f28d451b6533d52a9e74bee3c9d
Jason Beard <jwbeard@gmail.com>
2020-11-17 10:26:49 -0600

    Create install_lastpass.ps1

---
+++ Windows/install_lastpass/install_lastpass.ps1
@@ -0,0 +1,7 @@
+﻿$file = 'LastPassInstaller.msi'
+$link = 'https://download.cloud.lastpass.com/windows_installer/LastPassInstaller.msi'
+
+$ProgressPreference = 'silentlyContinue'
+Invoke-WebRequest($link) -OutFile ".\$file"
+
+Start-Process msiexec.exe -Wait -ArgumentList "/i $file /quiet"
