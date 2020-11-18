7c2648b45af8a00443c981296c1c78180a297cd7
Jason Beard <jwbeard@gmail.com>
2020-11-18 11:18:53 -0600

    updates

--- Windows/install_pulsesecure/install_pulsesecure.ps1
+++ Windows/install_pulsesecure/install_pulsesecure.ps1
@@ -1,5 +1,6 @@
 Start-Process msiexec.exe -Wait -ArgumentList "/i $psscriptroot\PulseSecure.x64.msi CONFIGFILE=$psscriptroot\Default.pulsepreconfig /qn /norestart"
 
-Start-Process "C:\Program Files (x86)\Common Files\Pulse Secure\JamUI\jamCommand.exe" -Wait -ArgumentList "-ImportFile Default.pulsepreconfig"
+Start-Sleep -Seconds 3
+Start-Process "C:\Program Files (x86)\Common Files\Pulse Secure\JamUI\jamCommand.exe" -Wait -ArgumentList "-ImportFile `"Default.pulsepreconfig`""
 Start-Sleep -Seconds 3
 Stop-Process "pulse.exe"
---
+++ Windows/install_slack/install_slack.ps1
@@ -0,0 +1,7 @@
+﻿$file = 'SlackSetup.exe'
+$link = "https://slack.com/ssb/download-win64"
+
+$ProgressPreference = 'silentlyContinue'
+Invoke-WebRequest($link) -OutFile ".\$file"
+
+Start-Process -FilePath .\$file -ArgumentList "-s"
