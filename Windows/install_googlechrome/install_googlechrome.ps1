8980ef4bc60801aa758082585bb82436a705bceb
Jason Beard <jwbeard@gmail.com>
2020-11-18 21:36:33 -0600

    blah

--- Windows/install_googlechrome/install_googlechrome.ps1
+++ Windows/install_googlechrome/install_googlechrome.ps1
@@ -1,5 +1,5 @@
-﻿$file = 'GoogleChromeStandaloneEnterprise.msi'
-$link = "http://dl.google.com/chrome/install/GoogleChromeStandaloneEnterprise.msi"
+﻿$file = 'GoogleChromeStandaloneEnterprise64.msi'
+$link = "http://dl.google.com/chrome/install/GoogleChromeStandaloneEnterprise64.msi"
 
 $ProgressPreference = 'silentlyContinue'
 Invoke-WebRequest($link) -OutFile ".\$file"
