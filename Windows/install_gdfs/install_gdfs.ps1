d0df63d8f17907e6d4dc36af0e61564a84ee7998
Jason Beard <jwbeard@gmail.com>
2020-11-18 21:49:27 -0600

    Update install_gdfs.ps1

--- Windows/install_gdfs/install_gdfs.ps1
+++ Windows/install_gdfs/install_gdfs.ps1
@@ -4,4 +4,4 @@ $link = "https://dl.google.com/drive-file-stream/GoogleDriveFSSetup.exe"
 $ProgressPreference = 'silentlyContinue'
 Invoke-WebRequest($link) -OutFile ".\$file"
 
-Start-Process -FilePath .\$file -Verb runAs -ArgumentList "--silent --desktop_shortcut --gsuite_shortcuts=false"
+Start-Process -FilePath .\$file -ArgumentList "--silent --desktop_shortcut --gsuite_shortcuts=false" -verb runAs -Wait
